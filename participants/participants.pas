fa0e85f9efa30182431de3a183dcfaf6dc2cc812
m2cci-RML <Robin.Miquel@etu.univ-grenoble-alpes.fr>
2020-03-05 16:17:40 +0100

    Modification du glossaire Personne -> Spectateur

--- participants/participants.pas
+++ participants/participants.pas
@@ -7,3 +7,18 @@
 
 participant model MyTheatre
 import glossary model from "../glossaries/glossaries.gls"
+
+//==========================================================================
+// "Class" level participants
+//==========================================================================
+// "Actors" are defined by UML usecase model ; they represent (classes of) users.
+// "Stakeholders" have some interest in the system and/or its development.
+// "Team roles" collaborate to design and develop the system/
+//==========================================================================
+
+// ----actors---------------------------------------------------------------
+actor Client
+      | Client est une `Personne`. Le role du Client est de consulter la 
+      | `Programmation` de `MyTheatre`.
+      | Pour realiser cette tâche, Client possède une application web.
+
