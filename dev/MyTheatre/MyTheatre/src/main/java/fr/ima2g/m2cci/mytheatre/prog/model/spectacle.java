93187b3b5e7ccf23edc951bc25077faf6e24de1d
m2cci-NMZ <nico100885@hotmail.com>
2020-03-06 10:30:47 +0100

    ajout du projet maven

---
+++ dev/MyTheatre/MyTheatre/src/main/java/fr/ima2g/m2cci/mytheatre/prog/model/spectacle.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ima2g.m2cci.mytheatre.prog.model;
+
+/**
+ *
+ * @author marti236
+ */
+public class spectacle {
+    
+}
