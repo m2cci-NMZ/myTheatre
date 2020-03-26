a56f61c3b349a080b42ffa703c7e93128723dea3
m2cci-RML <Robin.Miquel@etu.univ-grenoble-alpes.fr>
2020-03-26 16:04:32 +0100

    Vues fonctionnelles

--- bd/sql/jdd/jdd1.sql
+++ bd/sql/jdd/jdd1.sql
@@ -14,9 +14,7 @@ PRAGMA foreign_keys = ON;
 
 ---------------------------------------------------------------------------
 -- LesSpectacles
----------------------------------------------------------------------------
-
-
+--------------------------------------------------------------------------
 INSERT INTO LesSpectacles VALUES (46, 'Les Animaux', 9.5, 'unCinqAns', 'cirque');
 INSERT INTO LesSpectacles VALUES (20, 'L''avare', 10.0, 'toutPublic', 'humoristique');
 INSERT INTO LesHumoristiques VALUES (20, 0);
@@ -26,10 +24,39 @@ INSERT INTO LesSpectacles VALUES (17, 'Andromaque', 15.0, 'adulte', 'drame');
 --------------------------------------------------------------------------
 -- LesRepresentations_base
 ---------------------------------------------------------------------------
+INSERT INTO LesRepresentations_base VALUES('2020-03-13 18:00', 46, 0.0);
+INSERT INTO LesRepresentations_base VALUES('2020-03-15 20:00', 20, 0.2);
+INSERT INTO LesRepresentations_base VALUES('2020-03-13 20:00', 20, 0.0);
 
-INSERT INTO LesRepresentations_base VALUES('2020-03-13 18:00', 46,0.0);
-INSERT INTO LesRepresentations_base VALUES('2020-03-15 20:00', 20,0.2);
-INSERT INTO LesRepresentations_base VALUES('2020-03-13 20:00', 20,0.0);
+
+--------------------------------------------------------------------------
+-- LesRang
+----------------------------------------------------------------------
+INSERT INTO LesRangs VALUES (1);
+INSERT INTO LesRangs VALUES (5);
+
+
+--------------------------------------------------------------------------
+-- LesPlaces
+----------------------------------------------------------------------
+INSERT INTO LesPlaces VALUES (1, 5);
+INSERT INTO LesPlaces VALUES (1, 10);
+INSERT INTO LesPlaces VALUES (5, 40);
+
+
+--------------------------------------------------------------------------
+-- LesDossierAchats_base
+----------------------------------------------------------------------
+INSERT INTO LesDossiersAchats_base VALUES (1);
+INSERT INTO LesDossiersAchats_base VALUES (25);
+
+
+--------------------------------------------------------------------------
+-- LesTickets_base
+---------------------------------------------------------------------------
+INSERT INTO LesTickets_base VALUES ('2020-03-15 20:00', 1, 5, 3, '2020-03-10 18:20:10', 25);
+INSERT INTO LesTickets_base VALUES ('2020-03-13 18:00', 1, 5, 1, '2020-03-01 17:45:25', 1);
+INSERT INTO LesTickets_base VALUES ('2020-03-13 18:00', 1, 10, 2, '2020-03-05 14:45:35', 1);
 
 
 --------------------------------------------------------------------------
@@ -40,35 +67,7 @@ INSERT INTO LesUtilisateurs VALUES ('nico58','Martinez','Nicolas','nico58@gmail'
 INSERT INTO LesUtilisateurs VALUES ('celia18','kezmane','celia','celia18@gmail','admin');
 
 
---------------------------------------------------------------------------
--- LesTickets_base
----------------------------------------------------------------------------
-INSERT INTO LesTickets_base VALUES ('2020-03-15 20:00',1,5,3,'2020-03-10  18:20:10',25);
-INSERT INTO LesTickets_base VALUES ('2020-03-13 18:00',1,5,1,'2020-03-01  17:45:25',1);
-INSERT INTO LesTickets_base VALUES ('2020-03-13 18:00',1,10,2,'2020-03-05  14:45:35',1);
-
---------------------------------------------------------------------------
--- LesDossierAchats_base
-----------------------------------------------------------------------
-INSERT INTO LesDossierAchats_base VALUES (1);
-INSERT INTO LesDossierAchats_base VALUES (25);
-
-
---------------------------------------------------------------------------
--- LesPlaces
-----------------------------------------------------------------------
-INSERT INTO LesPlaces VALUES (5,1);
-INSERT INTO LesPlaces VALUES (10,1);
-INSERT INTO LesPlaces VALUES (40,5);
-
---------------------------------------------------------------------------
--- LesRang
-----------------------------------------------------------------------
-INSERT INTO LesRangs VALUES (1);
-INSERT INTO LesRangs VALUES (5);
-
-
---------------------------------------------------------------------------
+-------------------------------------------------------------------------
 -- LesTicketsReserves
 ----------------------------------------------------------------------
 INSERT INTO LesTicketsReserves VALUES (3,'robin32');
@@ -77,5 +76,6 @@ INSERT INTO LesTicketsReserves VALUES (3,'robin32');
 --------------------------------------------------------------------------
 -- LesTicketsAchetes
 ----------------------------------------------------------------------
-INSERT INTO LesTicketsReserves VALUES (1,'nico58');
-INSERT INTO LesTicketsReserves VALUES (2,'nico58');
+INSERT INTO LesTicketsAchetes VALUES (1,'nico58');
+INSERT INTO LesTicketsAchetes VALUES (2,'nico58');
+
--- bd/sql/schema/schema.sql
+++ bd/sql/schema/schema.sql
@@ -24,9 +24,22 @@ CREATE TABLE LesSpectacles(
 	CONSTRAINT DOM_Spe_typeSpe
 		CHECK (typeSpe in ("opera", "drame", "humoristique", "musical", "cirque")),
 	CONSTRAINT DOM_Spe_numeroSpe
-		CHECK ( 0 < numeroSpe),
+		CHECK (0 < numeroSpe),
 	CONSTRAINT DOM_Spe_prixDeBaseSpe
-		CHECK ( 0 < prixDeBaseSpe)
+		CHECK (0 < prixDeBaseSpe)
+);
+
+
+CREATE TABLE LesOperas(
+	numeroSpe INTEGER,
+	aUnOrchestreOpe INTEGER,
+
+	CONSTRAINT PK_Ope
+		PRIMARY KEY (numeroSpe),
+	CONSTRAINT FK_Ope_numeroSpe
+		FOREIGN KEY (numeroSpe) REFERENCES LesSpectacles(numeroSpe) ON DELETE CASCADE,
+	CONSTRAINT DOM_Ope_aUnOrchestre
+		CHECK (aUnOrchestreOpe in (0,1))
 );
 
 
@@ -43,25 +56,127 @@ CREATE TABLE LesHumoristiques(
 );
 
 
-CREATE TABLE LesOperas(
-	numeroSpe Integer,
-	aUnOrchestreOpe Integer,
-
-	CONSTRAINT PK_Ope
-		PRIMARY KEY (numeroSpe),
-	CONSTRAINT FK_Ope_numeroSpe
-		FOREIGN KEY (numeroSpe) REFERENCES LesSpectacles(numeroSpe) ON DELETE CASCADE,
-	CONSTRAINT DOM_Ope_aUnOrchestre
-		CHECK (aUnOrchestreOpe in (0,1))
-);
-
-
-CREATE TABLE LesRepresentations(
-	horaireRep VARCHAR(14),
+CREATE TABLE LesRepresentations_base(
+	horaireRep VARCHAR(16),
 	numeroSpe INTEGER,
+	tauxReducRep REAL,
 
 	CONSTRAINT PK_Rep
 		PRIMARY KEY (horaireRep),
 	CONSTRAINT FK_Rep_numeroSpe 
-		FOREIGN KEY (numeroSpe) REFERENCES LesSpectacles(numeroSpe) ON DELETE CASCADE
+		FOREIGN KEY (numeroSpe) REFERENCES LesSpectacles(numeroSpe) ON DELETE CASCADE,
+	CONSTRAINT DOM_Rep_tauxReducRep
+		CHECK (0 <= tauxReducRep AND tauxReducRep < 1)
 );
+
+
+CREATE TABLE LesRangs(
+	numeroRan INTEGER,
+	
+	CONSTRAINT PK_Ran
+		PRIMARY KEY (numeroRan),
+	CONSTRAINT DOM_Ran_numeroRan
+		CHECK (0 < numeroRan)
+);
+
+
+CREATE TABLE LesPlaces(
+	numeroRan INTEGER,
+	numeroPla INTEGER,
+
+	CONSTRAINT PK_Pla
+		PRIMARY KEY (numeroPla, numeroRan),
+	CONSTRAINT FK_Pla_numeroRan
+		FOREIGN KEY (numeroRan) REFERENCES LesRangs(numeroRan) ON DELETE CASCADE,
+	CONSTRAINT DOM_Pla_numeroPla
+		CHECK (0 < numeroPla)
+);
+
+
+CREATE TABLE LesDossiersAchats_base(
+	numeroDos INTEGER,
+
+	CONSTRAINT PK_Dos
+		PRIMARY KEY (numeroDos),
+	CONSTRAINT DOM_Dos_numeroDos
+		CHECK (0 < numeroDos)
+);
+
+
+CREATE TABLE LesTickets_base(
+	horaireRep VARCHAR(16),
+	numeroRan INTEGER,
+	numeroPla INTEGER,
+	numeroTic INTEGER,
+	dateEmissionTic VARCHAR (19),
+	numeroDos INTEGER,
+
+	CONSTRAINT PK_Tic
+		PRIMARY KEY (horaireRep, numeroRan, numeroPla),
+	CONSTRAINT UN_Tic_numeroTic
+		UNIQUE (numeroTic),
+	CONSTRAINT FK_Tic_horaireRep
+		FOREIGN KEY (horaireRep) REFERENCES LesRepresentations_base(horaireRep) ON DELETE CASCADE,
+	CONSTRAINT FK_Tic_numeroRan_numeroPla
+		FOREIGN KEY (numeroRan, numeroPla) REFERENCES LesPlaces(numeroRan, numeroPla) ON DELETE CASCADE,
+	CONSTRAINT DOM_Tic_numeroTic
+		CHECK (0 < numeroTic)
+);
+
+
+CREATE TABLE LesUtilisateurs(
+	loginUti VARCHAR(30),
+	nomUti VARCHAR(50),
+    prenomUti VARCHAR(50),
+	mailUti VARCHAR (50),
+	mdpUti VARCHAR (20),
+
+	CONSTRAINT PK_Uti
+		PRIMARY KEY (loginUti)
+);
+
+
+CREATE TABLE LesTicketsReserves(
+	numeroTic INTEGER,
+	loginUti VARCHAR (30),
+	
+	CONSTRAINT PK_TicR
+		PRIMARY KEY (numeroTic),
+	CONSTRAINT FK_TicR_numeroTic
+		FOREIGN KEY (numeroTic) REFERENCES LesTickets_base(numeroTic) ON DELETE CASCADE,
+    CONSTRAINT FK_TicR_loginUti
+		FOREIGN KEY (loginUti) REFERENCES LesUtilisateurs(loginUti) ON DELETE CASCADE
+
+);
+
+
+CREATE TABLE LesTicketsAchetes(
+	numeroTic INTEGER,
+	loginUti VARCHAR (30),
+
+	CONSTRAINT PK_TicA
+		PRIMARY KEY (numeroTic),
+	CONSTRAINT FK_TicA_numeroTic
+		FOREIGN KEY (numeroTic) REFERENCES LesTickets_base(numeroTic) ON DELETE CASCADE,
+    CONSTRAINT FK_TicA_loginUti
+		FOREIGN KEY (loginUti) REFERENCES LesUtilisateurs(loginUti) ON DELETE CASCADE
+);
+
+
+CREATE VIEW LesRepresentations(horaireRep, numeroSpe, tauxReducRep, placesDispoRep) AS
+	SELECT R.horaireRep, numeroSpe, tauxReducRep, 
+		((SELECT COUNT(numeroPla) FROM LesPlaces) - COUNT(numeroTic)) AS placesDispoRep
+	FROM LesRepresentations_base R LEFT OUTER JOIN LesTickets_base T ON (R.horaireRep = T.horaireRep)
+	GROUP BY R.horaireRep, numeroSpe, tauxReducRep ;
+
+
+CREATE VIEW LesTickets(horaireRep, numeroRan, numeroPla, numeroTic,	dateEmissionTic, numeroDos, prixTic) AS
+	SELECT R.horaireRep, T.numeroRan, T.numeroPla, T.numeroTic, T.dateEmissionTic, T.numeroDos, (1 - R.tauxReducRep)*S.prixDeBaseSpe AS prixTic
+	FROM LesTickets_base T JOIN LesRepresentations_base R ON (T.horaireRep = R.horaireRep)
+							JOIN LesSpectacles S ON (R.numeroSpe = S.numeroSpe) ;
+
+
+CREATE VIEW LesDossiersAchats(numeroDos, prixGlobalDos) AS 
+	SELECT DA.numeroDos, SUM(T.prixTic)
+	FROM LesDossiersAchats_base DA JOIN LesTickets T ON (DA.numeroDos = T.numeroDos)
+	GROUP BY DA.numeroDos ;
